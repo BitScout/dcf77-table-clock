6f201933bfae02d3eca7b0999ea4aaa07e0cfc2f
Christian Kollross <christian@kollross.name>
2023-09-16 18:33:36 +0200

    Simplified the code and avoided some false signals

--- DCF77-Table-Clock.ino
+++ DCF77-Table-Clock.ino
@@ -3,14 +3,13 @@
 // Bootup sequence:
 // ---------------------------
 // 1. Power delivery to DCF77 board switched on
-// 2. 30 second delay to let the module tune itself
-// 3. 7 seconds of finding the distinction between high and low
-// 4. The onboard LED starts blinking each second (100 or 200 ms each)
-// 5. After at least 60 seconds, the NeoPixel strip should start lighting up, one LED per second
+// 2. 3 second delay to let the module tune itself
+// 3. The onboard LED starts blinking each second (100 or 200 ms each)
+// 4. After at least 60 seconds, the NeoPixel strip should start lighting up, one LED per second
 
 // DCF77 board -> Arduino Nano
 // ---------------------------
-// VDD  -> D7 (For a cleaner power supply)
+// VDD  -> D7 (For a hopefully cleaner power supply, and to simplify the wiring)
 // GND  -> GND
 // Pin3 -> Nothing
 // Pin4 -> A7
@@ -23,27 +22,25 @@
 
 #define DCF77_POWER_PIN 7
 #define DCF77_DATA_PIN A7
-#define SAMPLEZIZE 100
 #define BITCOUNT 58
 
 #define LEDSPIN  6
 #define LEDCOUNT 60
 #define LED_OFFSET 59
 
-bool debug = true;
+bool debug = false;
 
 int signal = 0;
 int previousSignal = 0;
-int signalTogglePoint = 500;
 
-int signalSamples[SAMPLEZIZE];
-
+int signalTogglePoint = 512;
 long flankUpMillis = 0;
 long flankDownMillis = 0;
 long markMillis = 0;
 int  duration;
 int  bitCounter = 0;
 byte bitbuffer[BITCOUNT];
+bool bit = true;
 
 int minute  = 0;
 int hour    = 0;
@@ -62,12 +59,18 @@ bool dateParityOkay   = 1;
 
 int bitValues[8] = {1, 2, 4, 8, 10, 20, 40, 80};
 
+bool minuteSignalAquired = false;
+
 Adafruit_NeoPixel strip = Adafruit_NeoPixel(LEDCOUNT, LEDSPIN, NEO_GRB + NEO_KHZ800);
 
+uint32_t offBit  = strip.Color(0, 0, 0);
 uint32_t oneBit  = strip.Color(0, 255, 0);
-uint32_t zeroBit = strip.Color(0, 0, 255);
+uint32_t zeroBit = strip.Color(0, 0, 63);
 uint32_t errorBit = strip.Color(255, 0, 0);
 uint32_t successBit = strip.Color(150, 150, 150);
+uint32_t bootWaitingBit = strip.Color(0, 255, 0);
+uint32_t bootDoneBit = strip.Color(255, 255, 255);
+uint32_t bipBit = strip.Color(255, 255, 255);
 
 void setup(void) {
   Serial.begin(9600);
@@ -80,58 +83,57 @@ void setup(void) {
   clearStrip();
 
   digitalWrite(LED_BUILTIN, LOW);
-  delay(3000); // Give DCF77 module 30 seconds to tune itself
+  strip.setPixelColor(LED_OFFSET, bootWaitingBit);
+  strip.show();
+  delay(3000); // Give DCF77 module 60 seconds to tune itself
   digitalWrite(LED_BUILTIN, HIGH);
-  
-  int low;
-  int high;
-  
-  digitalWrite(LED_BUILTIN, HIGH);
-  for (int i = 0; i < SAMPLEZIZE; i = i + 1) {
-    signal = analogRead(DCF77_DATA_PIN);
-    signalSamples[i] = signal;
-    delay(70);
-  }
-  bubbleSort();
-  digitalWrite(LED_BUILTIN, LOW);
-
-  low = signalSamples[(int)(SAMPLEZIZE * 0.03)];
-  high = signalSamples[(int)(SAMPLEZIZE * 0.97)];
-  signalTogglePoint = low + (high - low) / 4;
-  previousSignal = low;
-  
-  if(debug) {
-    Serial.print("Detected levels: ");
-    Serial.print(low);
-    Serial.print(" / ");
-    Serial.print(signalTogglePoint);
-    Serial.print(" / ");
-    Serial.println(high);
-    Serial.println("Waiting for minute mark ...");
-  }
+  strip.setPixelColor(LED_OFFSET, bootDoneBit);
+  strip.show();
 }
 
 void loop(void) {
   signal = analogRead(DCF77_DATA_PIN);
-  //Serial.println(signal);
+  Serial.print(signal); Serial.print("\t");
+  Serial.print(1000); Serial.print("\t"); // Equivalent to 100 ms
+  Serial.print(2000); Serial.print("\t"); // Equivalent to 200 ms
+  Serial.println(duration); // Use serial plotter to watch this output
   
   if(signal > signalTogglePoint and previousSignal < signalTogglePoint) {
-    flankUpMillis = millis();
-    duration = (flankUpMillis - flankDownMillis);
-    digitalWrite(LED_BUILTIN, HIGH);
-    
-    if(duration > 1500 and duration < 2500) {
-      mark();
+    if(millis() > (flankUpMillis + 800)) {
+      flankUpMillis = millis();
+      duration = (flankUpMillis - flankDownMillis);
+      digitalWrite(LED_BUILTIN, HIGH);
+      strip.setPixelColor(LED_OFFSET, bipBit);
+      strip.show();
+      
+      if(duration > 1700 and duration < 2100) {
+        minuteSignalAquired = true;
+        mark();
+      }
     }
   } else if(signal < signalTogglePoint and previousSignal > signalTogglePoint) {
     flankDownMillis = millis();
     duration = (flankDownMillis - flankUpMillis);
-    digitalWrite(LED_BUILTIN, LOW);
 
-    bool bit = (duration > 150 and duration < 250);
+    digitalWrite(LED_BUILTIN, LOW);
+    strip.setPixelColor(LED_OFFSET, offBit);
+    strip.show();
+
+    bit = (duration > 140 and duration < 250);
     
     processBit(bit);
     bitCounter++;
+  }
+
+  if(minuteSignalAquired && ((millis() - flankUpMillis) > 10000)) {
+    digitalWrite(DCF77_POWER_PIN, LOW);
+    clearStrip();
+    delay(3000); // Give DCF77 module 3 seconds to lose power, then power it up again
+    digitalWrite(DCF77_POWER_PIN, HIGH);
+    delay(3000);
+
+    minuteSignalAquired = false;
+    flankUpMillis = millis();
   }
   
   previousSignal = signal;
@@ -270,21 +272,7 @@ void mark() {
 
 void clearStrip() {
   for(int i = 0; i < LEDCOUNT; i++) {
-    strip.setPixelColor(i, strip.Color(0, 0, 0));
+    strip.setPixelColor(i, offBit);
   }
   strip.show();
-}
-
-void bubbleSort() {
-  int out, in, swapper;
-  for(out=0 ; out < SAMPLEZIZE; out++) {  // outer loop
-    for(in=out; in<(SAMPLEZIZE-1); in++)  {  // inner loop
-      if( signalSamples[in] > signalSamples[in+1] ) {   // out of order?
-        // swap them:
-        swapper = signalSamples[in];
-        signalSamples [in] = signalSamples[in+1];
-        signalSamples[in+1] = swapper;
-      }
-    }
-  }
 }
